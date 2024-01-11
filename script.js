dc05d9495af138c38a3cfdc4687dc16a82be8f6d
Malay Thakur <malaythakur13@gmail.com>
2024-01-11 21:24:04 +0530

    Clear input field after task submission
    Set the value of the input field inside the 'newtask' element to an empty string, ensuring a clean input field for the next task entry

--- script.js
+++ script.js
@@ -20,8 +20,18 @@ document.querySelector('#push').onclick = function(){
 
         for(var i=0;i<current_tasks.length;i++){
             current_tasks[i].onclick = function(){
-                this.parentNode.remove()
+                this.parentNode.remove();
             }
         }
+
+        var tasks = document.querySelectorAll(".task");  
+        for(var i=0; i<tasks.length;i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed'); 
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+
     }
 }
